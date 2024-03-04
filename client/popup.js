44ba2150f03891a58a85091bec910c768d47cf8d
Lucas Tassi Souza <lucastassisouza@gmail.com>
2024-03-04 10:00:47 -0300

    first commit

---
+++ client/popup.js
@@ -0,0 +1,24 @@
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    var currentTab = tabs[0];
+    var pageIcon = document.getElementById('pageIcon');
+    var pageName = document.getElementById('pageName');
+    var message = document.getElementById('message');
+    var doneButton = document.getElementById('doneButton');
+    var editButton = document.getElementById('editButton');
+
+    pageIcon.src = currentTab.favIconUrl;
+    pageName.textContent = currentTab.title;
+
+    message.textContent = "Page Bookmarked!";
+
+    setTimeout(function() {
+      message.textContent = "";
+    }, 5000);
+
+    doneButton.addEventListener('click', function() {
+      window.close();
+    });
+
+    editButton.addEventListener('click', function() {
+    });
+  });
