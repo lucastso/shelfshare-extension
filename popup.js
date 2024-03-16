e3c6755f606b11441c379eda1f2993db6ac5e8f4
Lucas <lucastassisouza@gmail.com>
2024-03-16 18:14:10 -0300

    fixes

--- popup.js
+++ popup.js
@@ -1,15 +1,21 @@
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   var currentTab = tabs[0];
+
   var pageIcon = document.getElementById("pageIcon");
   var pageName = document.getElementById("pageName");
   var message = document.getElementById("message");
   var doneButton = document.getElementById("doneButton");
   var editButton = document.getElementById("editButton");
 
-  pageIcon.src = currentTab.favIconUrl;
-  pageName.textContent = currentTab.title;
-
-  message.textContent = "Page Bookmarked!";
+  if (currentTab.url == "chrome://newtab/") {
+    message.textContent = "You can't bookmark this tab!";
+    message.style.color = "red";
+    pageIcon.setAttribute("hidden", true);
+  } else {
+    pageIcon.src = currentTab.favIconUrl;
+    pageName.textContent = currentTab.title;
+    message.textContent = "Page Bookmarked!";
+  }
 
   setTimeout(function () {
     message.textContent = "";
@@ -20,6 +26,28 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   });
 
   editButton.addEventListener("click", function () {
-    window.open("http://localhost:3000/", "_blank").focus();
+    window.open("http://localhost:3000/", "_blank", "aaa=9").focus();
   });
+
+  // Dados a serem enviados
+  const data = {
+    user_id: 1,
+    folder_id: 1,
+    url: currentTab.url,
+    name: currentTab.title,
+    color: "#FAFAFA",
+    icon: currentTab.favIconUrl,
+  };
+
+  const endpointUrl = "http://localhost:5000/bookmark";
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(endpointUrl, requestOptions);
 });
